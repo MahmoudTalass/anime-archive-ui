b2f7ebf6a1584830b12fb0dbafa7ffc103205b9f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-02 19:51:40 -0500

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            dominant: "#17152F",
+            complementary: "#443F5D",
+            accent: "#B8ACCD",
+         },
+      },
+   },
+   plugins: [],
 };
