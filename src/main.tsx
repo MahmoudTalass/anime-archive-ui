947055923bc2e6eb64e9ce20433a138666fdc85f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-02 21:20:33 -0500

    Replace Home with FrontPage for the front page path

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router";
-import { Home } from "./home/Home.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { FrontPage } from "./frontpage/FrontPage.tsx";
 import { AuthProvider } from "./auth/AuthProvider.tsx";
 import { LoginForm } from "./auth/LoginForm.tsx";
 import { RegisterForm } from "./auth/RegisterForm.tsx";
 import { PageLayout } from "./PageLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
-   <StrictMode>
-      <AuthProvider>
-         <BrowserRouter>
-            <Routes>
-               <Route element={<PageLayout />}>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/login" element={<LoginForm />} />
-                  <Route path="/register" element={<RegisterForm />} />
-               </Route>
-            </Routes>
-
-         </BrowserRouter>
-      </AuthProvider>
-   </StrictMode>
+  <StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<PageLayout />}>
+            <Route path="/" element={<FrontPage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
 );
