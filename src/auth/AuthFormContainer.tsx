0e4f281390856c35ba4492573c457d94fbd5af16
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 14:47:42 -0500

    Separate the auth form page container from the form
    This provides flexiblility to add things to the auth pages that are other than the form inputs, like the submit button

--- src/auth/AuthFormContainer.tsx
+++ src/auth/AuthFormContainer.tsx
@@ -1,9 +1,11 @@
-export const AuthFormContainer = ({ children }: { children: React.JSX.Element[] }) => {
-   return (
-      <div className="flex justify-center ">
-         <section className="min-w-96 h-fit mt-32 bg-gradient-to-bl from-complementary from-30% to-[#8F84C3] rounded-lg p-8 flex flex-col gap-4">
-            {children}
-         </section>
-      </div>
-   );
+export const AuthFormContainer = ({
+  children,
+}: {
+  children: React.JSX.Element[];
+}) => {
+  return (
+    <section className="min-w-96 h-fit mt-32 bg-gradient-to-bl from-complementary from-30% to-[#8F84C3] rounded-lg p-8 flex flex-col gap-4">
+      {children}
+    </section>
+  );
 };
---
+++ src/auth/AuthFormPageContainer.tsx
@@ -0,0 +1,7 @@
+export const AuthFormPageContainer = ({
+  children,
+}: {
+  children: React.JSX.Element[];
+}) => {
+  return <div className="flex flex-col items-center">{children}</div>;
+};
