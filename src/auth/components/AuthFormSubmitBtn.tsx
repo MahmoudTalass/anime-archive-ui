f0f5e7be18beffb61f0a640367de19e32ea7451c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 16:19:17 -0500

    Add basic animation to auth submit button hover and click

--- src/auth/components/AuthFormSubmitBtn.tsx
+++ src/auth/components/AuthFormSubmitBtn.tsx
@@ -11,7 +11,7 @@ export const AuthFormSubmitBtn = ({
 }) => {
   return (
     <button
-      className="bg-gradient-to-tr from-[#B8ACCD] to-[#5C5667] mt-4 min-w-80 w-96 rounded-2xl text-2xl h-14"
+      className="bg-gradient-to-tr from-[#B8ACCD] to-[#5C5667] mt-4 min-w-80 w-96 rounded-2xl text-2xl h-14 hover:scale-[1.01] active:scale-[0.98] transition-transform"
       onClick={onClick}
       disabled={disabled}>
       {label}
