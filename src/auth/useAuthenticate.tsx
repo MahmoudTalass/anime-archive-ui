275ef38dc61a92cd964f1d14c532de639aaf69a2
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 14:38:09 -0500

    Create hook returning authentication state and auth handler

---
+++ src/auth/useAuthenticate.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { AuthContextType, useAuth, UserInfo } from "./AuthProvider";
+
+interface AuthSuccessResponseType {
+  data: {
+    token: string;
+    user: {
+      id: string;
+      username: string;
+      email: string;
+    };
+  };
+}
+
+interface AuthErrorResponseType {
+  error: {
+    message: string;
+    statusCode: number;
+  };
+}
+
+export const useAuthenticate = (authType: "register" | "login") => {
+  const { login, userInfo }: AuthContextType = useAuth() as AuthContextType;
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  const handleAuth = async (
+    email: string,
+    password: string,
+    username?: string
+  ) => {
+    setLoading(true);
+    setErrorMsg(null);
+    const authApiUrl = `http://localhost:3000/api/v1/auth/${authType}`;
+    let body: Record<string, any> = {
+      email,
+      password,
+    };
+    let headers: { Authorization?: string } = {};
+
+    if (authType == "register") {
+      body.username = username;
+    }
+
+    if (userInfo?.token) {
+      headers.Authorization = `Bearer ${userInfo.token}`;
+    }
+
+    try {
+      const response = await fetch(authApiUrl, {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers,
+      });
+
+      const responseBody: AuthSuccessResponseType | AuthErrorResponseType =
+        await response.json();
+
+      if ("error" in responseBody) {
+        if (!response.ok) {
+          if (
+            response.status === 403 ||
+            response.status === 400 ||
+            response.status === 401
+          ) {
+            throw new Error(responseBody.error.message);
+          } else {
+            throw new Error("Could not authenticate, please try again later.");
+          }
+        }
+      }
+
+      const userInfoResponse: AuthSuccessResponseType =
+        responseBody as AuthSuccessResponseType;
+
+      const userInfoObj: UserInfo = {
+        token: userInfoResponse.data.token,
+        email: userInfoResponse.data.user.email,
+        username: userInfoResponse.data.user.username,
+        id: userInfoResponse.data.user.id,
+      };
+
+      login(userInfoObj);
+    } catch (e) {
+      if (e instanceof Error) {
+        setErrorMsg(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { handleAuth, loading, errorMsg };
+};
