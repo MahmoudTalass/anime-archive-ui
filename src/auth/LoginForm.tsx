12061b0d0a9cc69ae5b66fcb2e738d6247b40f38
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 15:35:27 -0500

    Wrap the auth form with the auth form page container and add submit button

--- src/auth/LoginForm.tsx
+++ src/auth/LoginForm.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router";
 import { AuthFormContainer } from "./AuthFormContainer";
 import { AuthFormInput } from "./AuthFormInput";
 import React, { useState } from "react";
+import { AuthFormPageContainer } from "./AuthFormPageContainer";
+import { AuthFormSubmitBtn } from "./AuthFormSubmitBtn";
 
 export const LoginForm = () => {
   const [email, setEmail] = useState<string>("");
@@ -13,27 +15,30 @@ export const LoginForm = () => {
     setPassword(e.target.value);
 
   return (
-    <AuthFormContainer>
-      <AuthFormInput
-        inputTitle="Email"
-        inputType="email"
-        isRequired={true}
-        value={email}
-        onChange={handleEmailInput}
-      />
-      <AuthFormInput
-        inputTitle="Password"
-        inputType="password"
-        isRequired={true}
-        value={password}
-        onChange={handlePasswordInput}
-      />
-      <p className="mt-8 text-center">
-        Don't have an account?{" "}
-        <Link to="/register" className="underline">
-          Create an account
-        </Link>
-      </p>
-    </AuthFormContainer>
+    <AuthFormPageContainer>
+      <AuthFormContainer>
+        <AuthFormInput
+          inputTitle="Email"
+          inputType="email"
+          isRequired={true}
+          value={email}
+          onChange={handleEmailInput}
+        />
+        <AuthFormInput
+          inputTitle="Password"
+          inputType="password"
+          isRequired={true}
+          value={password}
+          onChange={handlePasswordInput}
+        />
+        <p className="mt-8 text-center">
+          Don't have an account?{" "}
+          <Link to="/register" className="underline">
+            Create an account
+          </Link>
+        </p>
+      </AuthFormContainer>
+      <AuthFormSubmitBtn label="Login" onClick={() => console.log()} />
+    </AuthFormPageContainer>
   );
 };
--- src/auth/RegisterForm.tsx
+++ src/auth/RegisterForm.tsx
@@ -2,11 +2,15 @@ import { Link } from "react-router";
 import { AuthFormContainer } from "./AuthFormContainer";
 import { AuthFormInput } from "./AuthFormInput";
 import { useState } from "react";
+import { useAuthenticate } from "./useAuthenticate";
+import { AuthFormPageContainer } from "./AuthFormPageContainer";
+import { AuthFormSubmitBtn } from "./AuthFormSubmitBtn";
 
 export const RegisterForm = () => {
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const { handleAuth, errorMsg, loading } = useAuthenticate("register");
 
   const handleUsernameInput = (e: React.ChangeEvent<HTMLInputElement>) =>
     setUsername(e.target.value);
@@ -16,34 +20,38 @@ export const RegisterForm = () => {
     setPassword(e.target.value);
 
   return (
-    <AuthFormContainer>
-      <AuthFormInput
-        inputTitle="Username"
-        inputType="text"
-        isRequired={true}
-        value={username}
-        onChange={handleUsernameInput}
-      />
-      <AuthFormInput
-        inputTitle="Email"
-        inputType="email"
-        isRequired={true}
-        value={email}
-        onChange={handleEmailInput}
-      />
-      <AuthFormInput
-        inputTitle="Password"
-        inputType="password"
-        isRequired={true}
-        value={password}
-        onChange={handlePasswordInput}
-      />
-      <p className="mt-8 text-center">
-        Already have an account?{" "}
-        <Link to="/login" className="underline">
-          Login
-        </Link>
-      </p>
-    </AuthFormContainer>
+    <AuthFormPageContainer>
+      <AuthFormContainer>
+        <AuthFormInput
+          inputTitle="Username"
+          inputType="text"
+          isRequired={true}
+          value={username}
+          onChange={handleUsernameInput}
+        />
+        <AuthFormInput
+          inputTitle="Email"
+          inputType="email"
+          isRequired={true}
+          value={email}
+          onChange={handleEmailInput}
+        />
+        <AuthFormInput
+          inputTitle="Password"
+          inputType="password"
+          isRequired={true}
+          value={password}
+          onChange={handlePasswordInput}
+        />
+        <p className="mt-8 text-center">
+          Already have an account?{" "}
+          <Link to="/login" className="underline">
+            Login
+          </Link>
+        </p>
+        <>{errorMsg && <p>{errorMsg}</p>}</>
+      </AuthFormContainer>
+      <AuthFormSubmitBtn label="Create Account" onClick={() => console.log()} />
+    </AuthFormPageContainer>
   );
 };
