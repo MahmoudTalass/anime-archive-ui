2c5500555433d60376da4cc27b16ac3dbba2c4a9
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 15:34:17 -0500

    Adjust width and min-width of the form  container

--- src/auth/AuthFormContainer.tsx
+++ src/auth/AuthFormContainer.tsx
@@ -4,7 +4,7 @@ export const AuthFormContainer = ({
   children: React.JSX.Element[];
 }) => {
   return (
-    <section className="min-w-96 h-fit mt-32 bg-gradient-to-bl from-complementary from-30% to-[#8F84C3] rounded-lg p-8 flex flex-col gap-4">
+    <section className="min-w-80 w-96 h-fit mt-32 bg-gradient-to-bl from-complementary from-30% to-[#8F84C3] rounded-lg p-8 flex flex-col gap-4">
       {children}
     </section>
   );
