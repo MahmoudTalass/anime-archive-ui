d1371ac7f45681606aa4392abd1f6c75dbb29dca
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-05 22:15:37 -0500

    Create route for anime browsing page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "./auth/AuthProvider.tsx";
 import { LoginForm } from "./auth/LoginForm.tsx";
 import { RegisterForm } from "./auth/RegisterForm.tsx";
 import { PageLayout } from "./PageLayout.tsx";
+import { BrowseAnimesPage } from "./browsePage/BrowseAnimesPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById("root")!).render(
             <Route path="/" element={<FrontPage />} />
             <Route path="/login" element={<LoginForm />} />
             <Route path="/register" element={<RegisterForm />} />
+            <Route path="/home" element={<BrowseAnimesPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
