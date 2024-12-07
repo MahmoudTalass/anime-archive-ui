3c23542a1421ea872aa47fd471ebb5f757dba341
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-07 11:24:58 -0500

    Only show authentication links if user isn't signed in

--- src/frontPage/FrontPage.tsx
+++ src/frontPage/FrontPage.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router";
 import homePageImg from "../assets/front-page-img.jpg";
+import { AuthContextType, useAuth } from "../auth/AuthProvider";
 
 export const FrontPage = (): React.JSX.Element => {
+  const { userInfo } = useAuth() as AuthContextType;
+
   return (
     <div className="flex flex-col items-center p-4">
       <section className="mt-16 max-w-[72rem] w-full bg-complementary p-12 flex gap-16 rounded-md shadow-sm">
@@ -23,16 +26,21 @@ export const FrontPage = (): React.JSX.Element => {
             <p className="text-5xl font-bold text-accent">Anime Archive</p>
           </div>
           <div className="font-bold flex flex-wrap gap-8">
-            <Link to="/login">
-              <button className="bg-transparent border-2 border-white rounded-xl w-32 p-2 hover:scale-105 transition-transform">
-                Login
-              </button>
-            </Link>
-            <Link to="/register">
-              <button className="bg-transparent border-2 border-white rounded-xl w-32 px-5 hover:scale-105 transition-transform">
-                Create account
-              </button>
-            </Link>
+            {!userInfo && (
+              <>
+                <Link to="/login">
+                  <button className="bg-transparent border-2 border-white rounded-xl w-32 p-2 hover:scale-105 transition-transform">
+                    Login
+                  </button>
+                </Link>
+                <Link to="/register">
+                  <button className="bg-transparent border-2 border-white rounded-xl w-32 px-5 hover:scale-105 transition-transform">
+                    Create account
+                  </button>
+                </Link>
+              </>
+            )}
+
             <Link to="/home">
               <button className="rounded-xl w-32 py-3 bg-accent hover:scale-105 transition-transform">
                 Browse Anime
