00faf2441e3beb5fabafe69ea74732dc39d912c8
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-09 19:21:56 -0500

    Create hover effect showing actions for an anime
    The actions are to allow user to add anime to their list or to view the anime details page

--- src/browsePage/AnimeCard.tsx
+++ src/browsePage/AnimeCard.tsx
@@ -1,16 +1,67 @@
 import { Link } from "react-router";
 import { IAnime } from "./types";
+import { AuthContextType, useAuth } from "../auth/AuthProvider";
+import { useState } from "react";
 
 export const AnimeCard = ({ anime }: { anime: Partial<IAnime> }) => {
+  const { userInfo } = useAuth() as AuthContextType;
+  const [hovering, setHovering] = useState(false);
+
+  const handleSwitchHoverStatus = () => {
+    if (userInfo) setHovering(!hovering);
+  };
+
+  const handleAddToList = async () => {
+    const url = "http://localhost:3000/api/v1/me/animes";
+    const headers = {
+      Authorization: `Bearer ${userInfo?.token}`,
+      "Content-Type": "application/json",
+    };
+    const body = {
+      malId: anime.malId,
+    };
+
+    try {
+      await fetch(url, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+      });
+    } catch (err) {
+      if (err instanceof Error) console.error(err.message);
+    }
+  };
+
   return (
-    <article className="w-52 text-center hover:font-bold transition">
-      <Link to={`/animes/${anime.malId}`}>
+    <article className="w-52 text-center hover:font-bold transition flex flex-col gap-2">
+      <div
+        className="w-fit hover:scale-[1.02] transition duration-150 relative"
+        onMouseEnter={handleSwitchHoverStatus}
+        onMouseLeave={handleSwitchHoverStatus}>
         <img
           src={anime.imageUrl}
           alt={`Poster for anime with title '${anime.title}'`}
-          className="w-full h-80 rounded-sm hover:scale-[1.01] transition duration-150"
+          className="w-full rounded-lg"
         />
-        <p>{anime.title}</p>
+        {hovering && (
+          <div className="absolute w-full inset-0 bg-black bg-opacity-50 transition duration-150 rounded-lg flex flex-col justify-center items-center gap-4">
+            <Link
+              to={`/animes/${anime.malId}`}
+              className="bg-dominant w-fit p-1 px-3 rounded-md">
+              View
+            </Link>
+            {userInfo && (
+              <button
+                className="bg-dominant w-fit p-1 px-3 rounded-md"
+                onClick={handleAddToList}>
+                Add to list
+              </button>
+            )}
+          </div>
+        )}
+      </div>
+      <Link to={`/animes/${anime.malId}`}>
+        <p className="duration-150">{anime.title}</p>{" "}
       </Link>
     </article>
   );
