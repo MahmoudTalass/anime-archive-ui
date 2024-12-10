f51dd1b5c1eb7b089d9051504ca9380a58988e82
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-09 19:56:40 -0500

    Show options on hover for anime entries in user list

--- src/userList/AnimeEntryCard.tsx
+++ src/userList/AnimeEntryCard.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { AuthContextType, useAuth } from "../auth/AuthProvider";
 import { IUserAnimeEntry } from "../browsePage/types";
+import { Link } from "react-router";
 
 export const AnimeEntryCard = ({
   animeEntry,
 }: {
   animeEntry: IUserAnimeEntry;
 }) => {
+  const { userInfo } = useAuth() as AuthContextType;
+  const [hovering, setHovering] = useState(false);
+
+  const handleSwitchHoverStatus = () => {
+    if (userInfo) setHovering(!hovering);
+  };
+
   let statusColor: "bg-[#1A9A1A]" | "bg-[#FFA500]" | "bg-[#3B82F6]";
 
   if (animeEntry.status === "completed") {
@@ -17,11 +27,29 @@ export const AnimeEntryCard = ({
 
   return (
     <article className="w-52 text-center hover:font-bold transition flex flex-col gap-2">
-      <img
-        src={animeEntry.animeDetails.imageUrl}
-        alt={`Poster for anime with title '${animeEntry.animeDetails.title}'`}
-        className="w-full rounded-lg hover:scale-[1.01] transition duration-150 "
-      />
+      <div
+        className="relative hover:scale-[1.02] transition duration-150"
+        onMouseEnter={handleSwitchHoverStatus}
+        onMouseLeave={handleSwitchHoverStatus}>
+        <img
+          src={animeEntry.animeDetails.imageUrl}
+          alt={`Poster for anime with title '${animeEntry.animeDetails.title}'`}
+          className="w-full rounded-lg "
+        />
+        {hovering && (
+          <div className="absolute w-full inset-0 bg-black bg-opacity-50 transition duration-150 rounded-lg flex flex-col justify-center items-center gap-4">
+            <button className="bg-dominant w-fit p-1 px-3 rounded-md">
+              Update
+            </button>
+
+            <Link to={`/animes/${animeEntry.malId}`}>
+              <button className="bg-dominant w-fit p-1 px-3 rounded-md">
+                View
+              </button>
+            </Link>
+          </div>
+        )}
+      </div>
       <div className="flex gap-4 items-center">
         <div
           className={`rounded-full ${statusColor} size-4 inline-block flex-shrink-0`}></div>
