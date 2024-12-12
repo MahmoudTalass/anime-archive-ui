bc1a12237128e52f08ebe674942f28626da69904
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-11 21:43:44 -0500

    Add dialog modal to contain entry updater form

--- src/userList/AnimeEntryCard.tsx
+++ src/userList/AnimeEntryCard.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { AuthContextType, useAuth } from "../auth/AuthProvider";
 import { IUserAnimeEntry } from "../browsePage/types";
 import { Link } from "react-router";
+import { UpdateEntryDetailsForm } from "./UpdateEntryDetailsFormModal";
 
 export const AnimeEntryCard = ({
   animeEntry,
+  updateEntryState,
+  deleteEntryState,
 }: {
   animeEntry: IUserAnimeEntry;
+  updateEntryState: (entry: IUserAnimeEntry) => void;
+  deleteEntryState: (malId: string) => void;
 }) => {
   const { userInfo } = useAuth() as AuthContextType;
   const [hovering, setHovering] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
 
   const handleSwitchHoverStatus = () => {
     if (userInfo) setHovering(!hovering);
@@ -25,39 +31,61 @@ export const AnimeEntryCard = ({
     statusColor = "bg-[#3B82F6]";
   }
 
-  return (
-    <article className="w-52 text-center hover:font-bold transition flex flex-col gap-2">
-      <div
-        className="relative hover:scale-[1.02] transition duration-150"
-        onMouseEnter={handleSwitchHoverStatus}
-        onMouseLeave={handleSwitchHoverStatus}>
-        <img
-          src={animeEntry.animeDetails.imageUrl}
-          alt={`Poster for anime with title '${animeEntry.animeDetails.title}'`}
-          className="w-full rounded-lg "
-        />
-        {hovering && (
-          <div className="absolute w-full inset-0 bg-black bg-opacity-50 transition duration-150 rounded-lg flex flex-col justify-center items-center gap-4">
-            <button className="bg-[#494395] w-fit p-1 px-3 rounded-md">
-              Update
-            </button>
+  const closeModal = () => {
+    dialogRef.current?.close();
+  };
+  const showModal = () => {
+    dialogRef.current?.showModal();
+  };
 
-            <Link to={`/animes/${animeEntry.malId}`}>
-              <button className="bg-[#494395] w-fit p-1 px-3 rounded-md">
-                View
-              </button>
-            </Link>
-          </div>
-        )}
-      </div>
-      <div className="flex gap-2 items-center">
+  return (
+    <>
+      <dialog
+        ref={dialogRef}
+        className="backdrop:bg-black backdrop:bg-opacity-50 w-full max-w-[550px] text-white rounded-lg">
+        <UpdateEntryDetailsForm
+          updateEntryState={updateEntryState}
+          deleteEntryState={deleteEntryState}
+          showModal={showModal}
+          closeModal={closeModal}
+          animeEntry={animeEntry}
+        />{" "}
+      </dialog>
+      <article className="w-52 text-center hover:font-bold transition flex flex-col gap-2">
         <div
-          className={`rounded-full ${statusColor} size-4 inline-block flex-shrink-0`}></div>
-        <p className="text-lg">{animeEntry.status}</p>
-      </div>
-      <p className="text-xl font-bold text-start">
-        {animeEntry.animeDetails.title}
-      </p>
-    </article>
+          className="relative hover:scale-[1.02] transition duration-150"
+          onMouseEnter={handleSwitchHoverStatus}
+          onMouseLeave={handleSwitchHoverStatus}>
+          <img
+            src={animeEntry.animeDetails.imageUrl}
+            alt={`Poster for anime with title '${animeEntry.animeDetails.title}'`}
+            className="w-full rounded-lg "
+          />
+          {hovering && (
+            <div className="absolute w-full inset-0 bg-black bg-opacity-50 transition duration-150 rounded-lg flex flex-col justify-center items-center gap-4">
+              <button
+                className="bg-[#494395] w-fit p-1 px-3 rounded-md"
+                onClick={showModal}>
+                Update
+              </button>
+
+              <Link to={`/animes/${animeEntry.malId}`}>
+                <button className="bg-[#494395] w-fit p-1 px-3 rounded-md">
+                  View
+                </button>
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className="flex gap-2 items-center">
+          <div
+            className={`rounded-full ${statusColor} size-4 inline-block flex-shrink-0`}></div>
+          <p className="text-lg">{animeEntry.status}</p>
+        </div>
+        <p className="text-xl font-bold text-start">
+          {animeEntry.animeDetails.title}
+        </p>
+      </article>
+    </>
   );
 };
