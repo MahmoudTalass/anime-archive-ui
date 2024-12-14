0fdb90f90a778b107f815d36215efaf354bbe5a1
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-14 13:01:13 -0500

    Set variables using const since they don't change

--- src/auth/useAuthenticate.tsx
+++ src/auth/useAuthenticate.tsx
@@ -3,89 +3,83 @@ import { AuthContextType, useAuth, UserInfo } from "./AuthProvider";
 import { APIErrorResponseType } from "../commonTypes";
 
 interface AuthSuccessResponseType {
-  data: {
-    token: string;
-    user: {
-      id: string;
-      username: string;
-      email: string;
-    };
-  };
+   data: {
+      token: string;
+      user: {
+         id: string;
+         username: string;
+         email: string;
+      };
+   };
 }
 
 export const useAuthenticate = (authType: "register" | "login") => {
-  const { login, userInfo }: AuthContextType = useAuth() as AuthContextType;
-  const [loading, setLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+   const { login, userInfo }: AuthContextType = useAuth() as AuthContextType;
+   const [loading, setLoading] = useState(false);
+   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
-  const handleAuth = async (
-    email: string,
-    password: string,
-    username?: string
-  ) => {
-    setLoading(true);
-    setErrorMsg(null);
-    const authApiUrl = `http://localhost:3000/api/v1/auth/${authType}`;
-    let body: Record<string, any> = {
-      email,
-      password,
-    };
-    let headers: { Authorization?: string; "Content-Type": string } = {
-      "Content-Type": "application/json",
-    };
-
-    if (authType == "register") {
-      body.username = username;
-    }
-
-    if (userInfo?.token) {
-      headers.Authorization = `Bearer ${userInfo.token}`;
-    }
-
-    try {
-      const response = await fetch(authApiUrl, {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers,
-      });
-
-      const responseBody: AuthSuccessResponseType | APIErrorResponseType =
-        await response.json();
-
-      if ("error" in responseBody) {
-        if (!response.ok) {
-          if (
-            response.status === 403 ||
-            response.status === 400 ||
-            response.status === 401 ||
-            response.status === 404
-          ) {
-            throw new Error(responseBody.error.message);
-          } else {
-            throw new Error("Could not authenticate, please try again later.");
-          }
-        }
-      }
-
-      const userInfoResponse: AuthSuccessResponseType =
-        responseBody as AuthSuccessResponseType;
-
-      const userInfoObj: UserInfo = {
-        token: userInfoResponse.data.token,
-        email: userInfoResponse.data.user.email,
-        username: userInfoResponse.data.user.username,
-        id: userInfoResponse.data.user.id,
+   const handleAuth = async (email: string, password: string, username?: string) => {
+      setLoading(true);
+      setErrorMsg(null);
+      const authApiUrl = `http://localhost:3000/api/v1/auth/${authType}`;
+      const body: Record<string, any> = {
+         email,
+         password,
+      };
+      const headers: { Authorization?: string; "Content-Type": string } = {
+         "Content-Type": "application/json",
       };
 
-      login(userInfoObj);
-    } catch (e) {
-      if (e instanceof Error) {
-        setErrorMsg(e.message);
+      if (authType == "register") {
+         body.username = username;
       }
-    } finally {
-      setLoading(false);
-    }
-  };
 
-  return { handleAuth, loading, errorMsg };
+      if (userInfo?.token) {
+         headers.Authorization = `Bearer ${userInfo.token}`;
+      }
+
+      try {
+         const response = await fetch(authApiUrl, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers,
+         });
+
+         const responseBody: AuthSuccessResponseType | APIErrorResponseType = await response.json();
+
+         if ("error" in responseBody) {
+            if (!response.ok) {
+               if (
+                  response.status === 403 ||
+                  response.status === 400 ||
+                  response.status === 401 ||
+                  response.status === 404
+               ) {
+                  throw new Error(responseBody.error.message);
+               } else {
+                  throw new Error("Could not authenticate, please try again later.");
+               }
+            }
+         }
+
+         const userInfoResponse: AuthSuccessResponseType = responseBody as AuthSuccessResponseType;
+
+         const userInfoObj: UserInfo = {
+            token: userInfoResponse.data.token,
+            email: userInfoResponse.data.user.email,
+            username: userInfoResponse.data.user.username,
+            id: userInfoResponse.data.user.id,
+         };
+
+         login(userInfoObj);
+      } catch (e) {
+         if (e instanceof Error) {
+            setErrorMsg(e.message);
+         }
+      } finally {
+         setLoading(false);
+      }
+   };
+
+   return { handleAuth, loading, errorMsg };
 };
