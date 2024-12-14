806577afcc4da7258f3948ffba41af82209c874c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-14 12:52:08 -0500

    Add navigate as a useEffect dependency

--- src/auth/RegisterForm.tsx
+++ src/auth/RegisterForm.tsx
@@ -10,86 +10,73 @@ import { AuthContextType, useAuth } from "./AuthProvider";
 const FORM_ID = "registrationForm";
 
 export const RegisterForm = () => {
-  const [username, setUsername] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const { handleAuth, errorMsg, loading } = useAuthenticate("register");
-  const { userInfo } = useAuth() as AuthContextType;
-  const navigate = useNavigate();
+   const [username, setUsername] = useState<string>("");
+   const [email, setEmail] = useState<string>("");
+   const [password, setPassword] = useState<string>("");
+   const { handleAuth, errorMsg, loading } = useAuthenticate("register");
+   const { userInfo } = useAuth() as AuthContextType;
+   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (userInfo) navigate("/");
-  }, [userInfo]);
+   useEffect(() => {
+      if (userInfo) navigate("/");
+   }, [userInfo, navigate]);
 
-  const handleUsernameInput = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setUsername(e.target.value);
-  const handleEmailInput = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setEmail(e.target.value);
-  const handlePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setPassword(e.target.value);
+   const handleUsernameInput = (e: React.ChangeEvent<HTMLInputElement>) =>
+      setUsername(e.target.value);
+   const handleEmailInput = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+   const handlePasswordInput = (e: React.ChangeEvent<HTMLInputElement>) =>
+      setPassword(e.target.value);
 
-  const onSubmit: React.FormEventHandler<HTMLFormElement> = (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-    if (
-      email.trim() === "" ||
-      username.trim() === "" ||
-      password.trim() === ""
-    ) {
-      return;
-    }
+   const onSubmit: React.FormEventHandler<HTMLFormElement> = (
+      e: React.FormEvent<HTMLFormElement>
+   ) => {
+      e.preventDefault();
+      if (email.trim() === "" || username.trim() === "" || password.trim() === "") {
+         return;
+      }
 
-    handleAuth(email, password, username);
+      handleAuth(email, password, username);
 
-    setPassword("");
-    if (!(typeof errorMsg === "string")) {
-      setEmail("");
-      setUsername("");
-    }
-  };
+      setPassword("");
+      if (!(typeof errorMsg === "string")) {
+         setEmail("");
+         setUsername("");
+      }
+   };
 
-  return (
-    <AuthFormPageContainer>
-      <AuthFormContainer onSubmit={onSubmit} formId={FORM_ID}>
-        <AuthFormInput
-          inputTitle="Username"
-          inputType="text"
-          isRequired={true}
-          value={username}
-          onChange={handleUsernameInput}
-        />
-        <AuthFormInput
-          inputTitle="Email"
-          inputType="email"
-          isRequired={true}
-          value={email}
-          onChange={handleEmailInput}
-        />
-        <AuthFormInput
-          inputTitle="Password"
-          inputType="password"
-          isRequired={true}
-          value={password}
-          onChange={handlePasswordInput}
-        />
-        <p className="mt-8 text-center">
-          Already have an account?{" "}
-          <Link to="/login" className="underline">
-            Login
-          </Link>
-        </p>
-        <>
-          {errorMsg && (
-            <p className="bg-red-600 p-2 text-center rounded-md">{errorMsg}</p>
-          )}
-        </>
-      </AuthFormContainer>
-      <AuthFormSubmitBtn
-        disabled={loading}
-        label="Create Account"
-        formAttr={FORM_ID}
-      />
-    </AuthFormPageContainer>
-  );
+   return (
+      <AuthFormPageContainer>
+         <AuthFormContainer onSubmit={onSubmit} formId={FORM_ID}>
+            <AuthFormInput
+               inputTitle="Username"
+               inputType="text"
+               isRequired={true}
+               value={username}
+               onChange={handleUsernameInput}
+            />
+            <AuthFormInput
+               inputTitle="Email"
+               inputType="email"
+               isRequired={true}
+               value={email}
+               onChange={handleEmailInput}
+            />
+            <AuthFormInput
+               inputTitle="Password"
+               inputType="password"
+               isRequired={true}
+               value={password}
+               onChange={handlePasswordInput}
+            />
+            <p className="mt-8 text-center">
+               Already have an account?{" "}
+               <Link to="/login" className="underline">
+                  Login
+               </Link>
+            </p>
+            <>{errorMsg && <p className="bg-red-600 p-2 text-center rounded-md">{errorMsg}</p>}</>
+         </AuthFormContainer>
+         <AuthFormSubmitBtn disabled={loading} label="Create Account" formAttr={FORM_ID} />
+      </AuthFormPageContainer>
+   );
 };
