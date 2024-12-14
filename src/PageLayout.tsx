e1c07e0591880bae20652c5c29065ffe3e4ccfd8
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-14 18:58:18 -0500

    Add app logo and specify the source in a footer

--- src/PageLayout.tsx
+++ src/PageLayout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router";
 
 export const PageLayout = () => {
-  return (
-    <main>
-      <Outlet />
-    </main>
-  );
+   return (
+      <main className="flex flex-col min-h-full">
+         <Outlet />
+         <footer className="flex-grow-0 mt-auto text-center text-gray-400 p-2 text-xs">
+            Logo by{" "}
+            <a href="https://icons8.com/" className="underline">
+               Icon8
+            </a>
+         </footer>
+      </main>
+   );
 };
--- src/commonComponents/Header.tsx
+++ src/commonComponents/Header.tsx
@@ -4,76 +4,72 @@ import React, { useState } from "react";
 import { AuthContextType, useAuth } from "../auth/AuthProvider";
 import { MdFormatListBulleted } from "react-icons/md";
 
-export const Header = ({
-  children,
-}: {
-  children?: React.JSX.Element | React.JSX.Element[];
-}) => {
-  const { userInfo, logout } = useAuth() as AuthContextType;
-  const navigate = useNavigate();
-  const [getRandomLoading, setGetRandomLoading] = useState(false);
+export const Header = ({ children }: { children?: React.JSX.Element | React.JSX.Element[] }) => {
+   const { userInfo, logout } = useAuth() as AuthContextType;
+   const navigate = useNavigate();
+   const [getRandomLoading, setGetRandomLoading] = useState(false);
 
-  const handleRandomAnimeBtn = async () => {
-    setGetRandomLoading(true);
-    try {
-      const response = await fetch(
-        "http://localhost:3000/api/v1/animes/random"
-      );
-      const data: { data: { malId: number } } = await response.json();
-      if (response.ok) {
-        navigate(`/animes/${data.data.malId}`);
+   const handleRandomAnimeBtn = async () => {
+      setGetRandomLoading(true);
+      try {
+         const response = await fetch("http://localhost:3000/api/v1/animes/random");
+         const data: { data: { malId: number } } = await response.json();
+         if (response.ok) {
+            navigate(`/animes/${data.data.malId}`);
+         }
+      } catch (err) {
+         if (err instanceof Error) console.log(err.message);
+      } finally {
+         setGetRandomLoading(false);
       }
-    } catch (err) {
-    } finally {
-      setGetRandomLoading(false);
-    }
-  };
+   };
 
-  const handleLogoutUser = () => {
-    logout();
-  };
+   const handleLogoutUser = () => {
+      logout();
+   };
 
-  return (
-    <div className="flex justify-around items-center w-full p-4 py-8">
-      <Link to="/home">
-        <h1 className="text-5xl font-bold">
-          Anime <span className="text-complementary">Archive</span>
-        </h1>
-      </Link>
-      {children}
-      <button
-        className="flex items-center gap-2"
-        onClick={handleRandomAnimeBtn}
-        disabled={getRandomLoading}>
-        <FaRandom size="1.3rem" />
-        <p>Random</p>
-      </button>
-      {userInfo ? (
-        <>
-          <Link to="/mylist" className="text-lg flex items-center gap-1">
-            <MdFormatListBulleted size="1.5rem" />
-            My List
-          </Link>
-          <button
-            className="bg-otherPurple px-4 py-3 rounded-xl font-bold"
-            onClick={handleLogoutUser}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <div className="flex gap-4">
-          <Link to="/login">
-            <button className="bg-otherPurple px-4 py-3 rounded-xl font-bold">
-              Login
-            </button>
-          </Link>
-          <Link to="/register">
-            <button className="bg-otherPurple px-4 py-3 rounded-xl font-bold">
-              Register
-            </button>
-          </Link>
-        </div>
-      )}
-    </div>
-  );
+   return (
+      <div className="flex flex-col gap-5 md:gap-0 md:flex-row md:justify-around items-center w-full p-4 py-4">
+         <Link to="/home" className="flex items-center">
+            <img src="/animeIcon.svg" alt="app icon by Icon8" />
+            <h1 className="text-5xl font-bold">
+               Anime <span className="text-complementary">Archive</span>
+            </h1>
+         </Link>
+         {children}
+         <button
+            className="flex items-center gap-2"
+            onClick={handleRandomAnimeBtn}
+            disabled={getRandomLoading}
+         >
+            <FaRandom size="1.3rem" />
+            <p>Random</p>
+         </button>
+         {userInfo ? (
+            <>
+               <Link to="/mylist" className="text-lg flex items-center gap-1">
+                  <MdFormatListBulleted size="1.5rem" />
+                  My List
+               </Link>
+               <button
+                  className="bg-otherPurple px-4 py-3 rounded-xl font-bold"
+                  onClick={handleLogoutUser}
+               >
+                  Logout
+               </button>
+            </>
+         ) : (
+            <div className="flex gap-4">
+               <Link to="/login">
+                  <button className="bg-otherPurple px-4 py-3 rounded-xl font-bold">Login</button>
+               </Link>
+               <Link to="/register">
+                  <button className="bg-otherPurple px-4 py-3 rounded-xl font-bold">
+                     Register
+                  </button>
+               </Link>
+            </div>
+         )}
+      </div>
+   );
 };
