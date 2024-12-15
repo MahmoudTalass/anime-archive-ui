f3ee18c22bc729826b220b737314c7a5d5ef9f0f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-12-14 20:31:16 -0500

    Remove unused import

--- src/auth/components/AuthFormSubmitBtn.tsx
+++ src/auth/components/AuthFormSubmitBtn.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-
 export const AuthFormSubmitBtn = ({
-  label,
-  disabled = false,
-  formAttr,
+   label,
+   disabled = false,
+   formAttr,
 }: {
-  label: string;
-  disabled?: boolean;
-  formAttr: string;
+   label: string;
+   disabled?: boolean;
+   formAttr: string;
 }) => {
-  return (
-    <button
-      form={formAttr}
-      className="bg-gradient-to-tr from-[#B8ACCD] to-[#5C5667] mt-4 min-w-80 w-96 rounded-2xl text-2xl h-14 hover:scale-[1.01] active:scale-[0.98] transition-transform"
-      disabled={disabled}
-      type="submit">
-      {label}
-    </button>
-  );
+   return (
+      <button
+         form={formAttr}
+         className="bg-gradient-to-tr from-[#B8ACCD] to-[#5C5667] mt-4 min-w-80 w-96 rounded-2xl text-2xl h-14 hover:scale-[1.01] active:scale-[0.98] transition-transform"
+         disabled={disabled}
+         type="submit"
+      >
+         {label}
+      </button>
+   );
 };
